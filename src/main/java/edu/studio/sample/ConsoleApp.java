3a813b10b22a5c330fde7484cb655f6f7534268f
Caleb Magoola <cmagoo01@villanova.edu>
2026-04-02 16:51:33 -0400

    Quick Update to Test Github Actions

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -5,6 +5,7 @@ public class ConsoleApp {
     public static void main(String[] args) {
         
         System.out.println("Hello Maven");
+        System.out.println("This is a test to check how GitHub Actions works.");
         
     }
 
